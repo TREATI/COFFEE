0d172147a64de24383c6c070ae55b4fcc889b69a
Victor Prüfer <victor.pruefer@containergrid.de>
2021-05-07 10:46:36 +0200

    Remove unused files

--- Sources/COFFEE/Model/Submission.swift
+++ Sources/COFFEE/Model/Submission.swift
@@ -8,9 +8,24 @@
 import Foundation
 
 /// A submission groups multiple item responses
-public struct Submission {
+public struct Submission: Codable {
     public let submissionDate: Date
     public let responses: [ItemResponse]
+    
+    /// The coding keys for a text item
+    enum CodingKeys: String, CodingKey {
+        case submissionDate
+        case responses
+    }
+    
+    /// Creates a new instance of submission from a decoder
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+                
+        // Decode all required values
+        self.submissionDate = try container.decode(Date.self, forKey: .submissionDate)
+        self.responses = try container.decode([ItemResponse].self, forKey: .responses)
+    }
 }
 
 extension Submission: CustomStringConvertible {
